cc3195de97d22ea2dc752ab68b4b7005405c449b
KoushalGH <koushalgh11@gmail.com>
2025-07-26 15:28:30 +0530

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,6 @@
+import App from "./App";
+import ReactDom from "react-dom/client";
+import React from "react";
+
+const root=ReactDom.createRoot(document.getElementById("root"))
+root.render(<App />)
